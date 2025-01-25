10ba14de13a7c42587b24005bd996d3e46326df8
SimranBeedi <106719750+SimranBeedi@users.noreply.github.com>
2025-01-25 10:20:37 +0530

    Update assets.js

--- assets/assets.js
+++ assets/assets.js
@@ -105,7 +105,8 @@ export const workData = [
         
             title: 'Responsive Portfolio Website',
             description: 'Built a dynamic and responsive portfolio for showcasing projects',
-            bgImage: '/assets/work-1.png
+            bgImage: '/assets/work-1.png',
+
             link: 'https://yourportfolio.com', // Replace with your live project or GitHub repo link
           
           
